426106ed4e342880db676fa549ae29be89ae492a
Gabriela Garza-Long <gabriela.garza2@my.utsa.edu>
2025-05-06 21:21:49 -0500

    ..

--- Lab2_Assignment.R
+++ Lab2_Assignment.R
@@ -9,28 +9,84 @@
 # 2. [60 points] Open the R file "Lab2_Assignment.R" and answer the questions
 
 #---- Q1. write the code to load the dataset "tract_covariates.csv" located under the "datasets" folder in your repository. Create an object called `opportunities` Use the data.table package to do this. ----
-
-
+opportunities <-tract_covariates
 #---- Q2. On your browser, read and become familiar with the dataset metadata. Next write the code for the following:
 # Link to metadata: https://opportunityinsights.org/wp-content/uploads/2019/07/Codebook-for-Table-9.pdf 
+install.packages(c("httr", "pdftools"))
+library(httr)
+library(pdftools)
+pdf_url <- "https://opportunityinsights.org/wp-content/uploads/2019/07/Codebook-for-Table-9.pdf"
+pdf_path <- "Codebook-for-Table-9.pdf"
+GET(pdf_url, write_disk(pdf_path, overwrite = TRUE))
+pdf_text_content <- pdf_text(pdf_path)
+cat(pdf_text_content[1])
+pdf_tables <- pdf_data(pdf_path)
+print(pdf_tables)
+
+
 
 # what is the object class?
-
+class(opportunities) 
+#class is character
 # how can I know the variable names?
-
+variable.names(tract_covariates)
+ #names(opportunities) will show names in console
 # What is the unit of analysis? 
+head(opportunities)
+#Units of analysis would be : Household, Individuals, State, Tract, County
 
 # Use the `summary` function to describe the data. What is the variable that provides more interest to you?
-
+summary(opportunities) #I gravitate towards population density and demographics such as ethnicities
 # Create a new object called `sa_opportunities` that only contains the rows for the San Antonio area (hint: use the `czname` variable). 
-
+sa_opportunities <- tract_covariates[tract_covariates$czname == "San Antonio", ]
+head(sa_opportunities)
 
 # Create a plot that shows the ranking of the top 10 census tracts by Annualized job growth rate (`ann_avg_job_growth_2004_2013` variable) by census tract (tract variable). Save the resulting plot as a pdf with the name 'githubusername_p1.pdf' # Hint: for ordering you could use the `setorderv()` or reorder() functions, and the ggsave() function to export the plot to pdf. 
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(tidyverse)
+library(leaflet)
+library(tigris)
+
+setDT(opportunities)
+setorderv(opportunities, "ann_avg_job_growth_2004_2013", order = -1)
+top_10 <- opportunities[1:10, ]
+View(top_10)
+
+top_10 <- opportunities %>% 
+  filter(!is.na(ann_avg_job_growth_2004_2013)) %>%
+  arrange(desc(ann_avg_job_growth_2004_2013)) %>% head(10)
+
+p <- ggplot(top_10, aes(x = reorder(tract, ann_avg_job_growth_2004_2013), 
+                        y = ann_avg_job_growth_2004_2013)) +
+  geom_col(fill = "steelblue") +
+  coord_flip() +  
+  labs(title = "Top 10 Census Tracts by Annualized Job Growth Rate (2004-2013)",
+       x = "Census Tract",
+       y = "Annualized Job Growth Rate") +
+  theme_minimal()
+  
+ggsave("greenrange_p1.pdf", plot = p, width = , height = )
+#my plots view don't generate, even when code runs, the pdf shows the plot, i did restart my computer, checked updates were made 
 
 # Create a plot that shows the relation between the `frac_coll_plus` and the `hhinc_mean2000` variables, what can you hypothesize from this relation? what is the causality direction? Save the resulting plot as a pdf with the name 'githubusername_p3.pdf'
+ggplot(opportunities, aes(x = frac_coll_plus2000, y = hhinc_mean2000)) +
+  geom_point(alpha = 0.6, color = "blue") +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(
+    x = "Fraction with College Degree or Higher",
+    y = "Mean Household Income in 2000",
+    title = "Relationship between Education Level and Household Income"
+  ) +
+  theme_minimal()
+ggsave("greenrange_p3.pdf", plot = p, width = , height = )
+
+#Hypothesis: Individuals with a college degree or higher tend to make more money/ have a higher household income
+#Causality: There is a positive correlation trend. However, Correlation does not equal Causation: there a multitude of factors that can affect either (socieconomic status, regional opportunities, etc)
 
 # Investigate (on the internet) how to add a title,a subtitle and a caption to your last plot. Create a new plot with that and save it as 'githubusername_p_extra.pdf'
-
+#I jumped the gun, so my previous plot has a title 
 
 
 
--- Lab2_Script.R
+++ Lab2_Script.R
@@ -2,8 +2,6 @@
 # The University of Texas at San Antonio
 # URP-5393: Urban Planning Methods II
 
-#test
-#test222
 #---- Objectives ----
 # In this Lab you will learn to:
 
@@ -21,41 +19,44 @@
 ## ---- Part 1.1: Loading data from R pre-loaded packages ----
 
 data() # shows all preloaded data available in R in the datasets package
-help(package="datasets")
+help(package="datasets") #Function will load datasets to provide help 
 
 #Let's us the Violent Crime Rates by US State data 
 
-help("USArrests")
+help("USArrests") #function loads the data for arrests 
 
 # Step 1. Load the data in you session by creating an object
 
 usa_arrests<-datasets::USArrests # this looks the object 'USAarrests' within '::' the package 'datasets'
 
-class(usa_arrests)
-names(usa_arrests)
-dim(usa_arrests)
-head(usa_arrests)
-tail(usa_arrests)
+class(usa_arrests) #Class function lets me know that "usa_arrests" is a data.frame 
+names(usa_arrests)#names pulls the list of column names in this dataset
+dim(usa_arrests)#pulls the matrix from the dataset usa_arrests, 50 (states) and 4 Columns 
+head(usa_arrests) #quick preview of initial start of the dataset
+tail(usa_arrests) #quick preview of the end of the dataset 
+summary(usa_arrests)
 
 ## ---- Part 1.2: Loading data from your computer directory ----
 # We will use the Building Permits data from the city of San Antonio open data portal
 # Source: https://data.sanantonio.gov/dataset/building-permits/resource/c21106f9-3ef5-4f3a-8604-f992b4db7512
+> library(readr)
+> accelaissuedpermitsextract <- read_csv("02-lab-assignment-Green-range/datasets/accelaissuedpermitsextract.csv")
+building_permits_sa<-read.csv(file ="datasets/accelaissuedpermitsextract.csv",header = T) #opens dataset (although it is not )
 
-building_permits_sa<-read.csv(file = "datasets/accelaissuedpermitsextract.csv",header = T)
-
-names(building_permits_sa)
-View(building_permits_sa)
-class(building_permits_sa)
-dim(building_permits_sa)
-str(building_permits_sa)
-summary(building_permits_sa)
+names(building_permits_sa) #class function lets me knw the list of the names in the dataset
+View(building_permits_sa) #open the dataset on a different tab 
+class(building_permits_sa) #tells me what kind of class (ex: dataset, function, etc... )
+dim(building_permits_sa) #shows me the end of the list of the matrix
+str(building_permits_sa) #gives me a structured summary of the dataset
+summary(building_permits_sa) #give overview of statistics
 
 
 ## ---- Part 1.3: Loading data directly from the internet ----
 # We will use the Building Permits data from the city of San Antonio open data portal
 # Source: https://data.sanantonio.gov/dataset/building-permits/resource/c21106f9-3ef5-4f3a-8604-f992b4db7512
 
-building_permits_sa2 <- read.csv("https://data.sanantonio.gov/dataset/05012dcb-ba1b-4ade-b5f3-7403bc7f52eb/resource/c21106f9-3ef5-4f3a-8604-f992b4db7512/download/accelaissuedpermitsextract.csv",header = T) 
+building_permits_sa2 <- read.csv("https://data.sanantonio.gov/dataset/05012dcb-ba1b-4ade-b5f3-7403bc7f52eb/resource/c21106f9-3ef5-4f3a-8604-f992b4db7512/download/accelaissuedpermitsextract.csv",header = T)
+
 
 
 
@@ -63,85 +64,90 @@ building_permits_sa2 <- read.csv("https://data.sanantonio.gov/dataset/05012dcb-b
 ## ---- Part 1.4: Loading data using a package + API ----
 #install.packages("tidycensus")
 #install.packages("tigris")
-help(package="tidycensus")
-library(tidycensus)
-library(tigris)
+help(package="tidycensus") # loads database on all packages available and gives help as 
+library(tidycensus) #function library loads and installs the package -in this case tidycensus
+library(tigris) #function library loads and installs the package -in this case tigris
 
 
 #type ?census_api_key to get your Census API for full access.
 
 age10 <- get_decennial(geography = "state", 
                        variables = "P013001", 
-                       year = 2010)
+                       year = 2010) #age 10 is the object name, get decenial is a tidycensus package function that pull ups data (demographics,housing,population, etc) from the USCensusDecenial 
+#Census Bureau in this case, state, median age, and year
 
-head(age10)
+head(age10) #head is the function and age 10 is the object assigned to include the state/ median age/ and year, using head function will give you the first 6 rows of the matrix 
 
 
 bexar_medincome <- get_acs(geography = "tract", variables = "B19013_001",
-                           state = "TX", county = "Bexar", geometry = TRUE)
+                           state = "TX", county = "Bexar", geometry = TRUE) #bexar_medincome is the object with has been assigned to pull data through the function: get_acs from the american community survey
 
 
-View(bexar_medincome)
+View(bexar_medincome) #view is the function that opens the data into another tab
 
-class(bexar_medincome)
+class(bexar_medincome) #the class function tells you what type of information you pull, in this case, bexar_medincome is a data frame
 
 #---- Part 2: Visualizing the data ----
 #install.packages('ggplot2')
 
-library(ggplot2)
+library(ggplot2) #loads and installs package ggplot2
 
 
 
 ## ---- Part 2.1: Visualizing the 'usa_arrests' data ----
 
-ggplot()
+ggplot(usa_arrests)  #function provides a blank page, 
+ #function provides a blank page, nothing shows up for usa_arrrests
 
 #scatter plot - relationship between two continuous variables
 ggplot(data = usa_arrests,mapping = aes(x=Assault,y=Murder)) +
-  geom_point()
+  geom_point() #ggplot function pulls up data of assualt and murders and mapped it in in a scatter plot
 
 ggplot() +
-  geom_point(data = usa_arrests,mapping = aes(x=Assault,y=Murder))
+  geom_point(data = usa_arrests,mapping = aes(x=Assault,y=Murder)) #this is another way to get the same scatter plot
 
 
 #bar plot - compare levels across observations
-usa_arrests$state<-rownames(usa_arrests)
+usa_arrests$state<-rownames(usa_arrests) #usa_arrests object gets added through a dollar sign -states, to the row names of the data set
 
 ggplot(data = usa_arrests,aes(x=state,y=Murder))+
   geom_bar(stat = 'identity')
 
 ggplot(data = usa_arrests,aes(x=reorder(state,Murder),y=Murder))+
   geom_bar(stat = 'identity')+
-  coord_flip()
+  coord_flip() #ggplot function  pulls the data from us_arrest  and pulls murder by state  in a bar graph
 
 # adding color # would murder arrests be related to the percentage of urban population in the state?
 ggplot(data = usa_arrests,aes(x=reorder(state,Murder),y=Murder,fill=UrbanPop))+
   geom_bar(stat = 'identity')+
-  coord_flip()
+  coord_flip() #ggplot function  pulls the data from us_arrest  and pulls murder by state  in a bar graph, this time adding fill- urban pop generates colorscheme
 
 # adding size
 ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop)) +
-  geom_point()
+  geom_point() #ggplot function  pulls the data from us_arrest  and pulls murder by state  in a plot graph, adding size to urban pop changes the sizes of the dots in plots to correlate to data
 
 
 # plotting by south-east and everyone else 
 
-usa_arrests$southeast<-as.numeric(usa_arrests$state%in%c("Florida","Georgia","Mississippi","Lousiana","South Carolina"))
+usa_arrests$southeast<-as.numeric(usa_arrests$state%in%c("Florida","Georgia","Mississippi","Louisiana","South Carolina")) 
+# usa_arrests object adds the southeast through dollar, the as.numeric function assigns true/ false as 1 or 0  as in state is or is not in the southeast
+
 
 
 ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop, color=southeast)) +
-  geom_point()
+  geom_point() #ggplot functions puts the data in usa_arrests, into geometric point graph, size-urban pop changes size of dots, while color is added only to southeast
 
-usa_arrests$southeast<-factor(usa_arrests$southeast,levels = c(1,0),labels = c("South-east state",'other'))
-
-ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop)) +
-  geom_point()+
-  facet_wrap(southeast~ .)
+usa_arrests$southeast<-factor(usa_arrests$southeast,levels = c(1,0),labels = c("South-east state",'other')) #this code adds 
 
 
 ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop)) +
   geom_point()+
-  facet_grid(southeast ~ .)
+  facet_wrap(southeast~ .) #plots are wrapped into a single line, or a new one if necessary, the facet wrap function creates separate plots "facets" for each level in a wrapped layout
+
+
+ggplot(data = usa_arrests,aes(x=Assault,y=Murder, size=UrbanPop)) +
+  geom_point()+
+  facet_grid(southeast ~ .) #plots are in a strict row column/ facet grid creates a grid of plots, using one variable for rows and another one for columns 
 
 ## ---- Part 3: Visualizing the spatial data ----
 # Administrative boundaries
@@ -181,6 +187,7 @@ ggsave(filename = "02_lab/plots/01_static_map.pdf",plot = p1) #saves the plot as
 
 #install.packages("mapview")
 library(mapview)
+
 
 mapview(bexar_county)
 
